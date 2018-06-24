7eeb7dc77e86cc7805b1840566c2fd6d518442ab
Amy Thompson <amythompson9@gmail.com>
2018-06-23 22:10:56 -0400

    first commit

---
+++ UdemyToDoList2/JavaScript.js
@@ -0,0 +1,18 @@
+﻿var todos = [];
+var input = prompt("What would you like to do?");
+
+while (input !== "quit") {
+    //handle input
+    if (input === "list") {
+        console.log(todos);
+    }
+    else if (input === "new") {
+        //ask for new todo
+        var newTodo = prompt("Enter new To Do");
+        //add to todos array
+        todos.push(newToDo);
+    }
+    //ask again for new input
+    var input = prompt("What would you like to do?");
+}
+console.log("Ok. You quit the app.");
