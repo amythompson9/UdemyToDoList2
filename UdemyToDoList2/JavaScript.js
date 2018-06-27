b732901f74d7a02f42c53eae67fc106988b213d2
Amy Thompson <amythompson9@gmail.com>
2018-06-27 19:02:17 -0400

    Delete Function

--- UdemyToDoList2/JavaScript.js
+++ UdemyToDoList2/JavaScript.js
@@ -4,15 +4,40 @@ var input = prompt("What would you like to do?");
 while (input !== "quit") {
     //handle input
     if (input === "list") {
-        console.log(todos);
+        listTodos();
     }
     else if (input === "new") {
-        //ask for new todo
-        var newTodo = prompt("Enter new To Do");
-        //add to todos array
-        todos.push(newToDo);
+        addTodo();
+    }
+    else if (input === "delete") {
+        deleteTodo();
     }
     //ask again for new input
     var input = prompt("What would you like to do?");
 }
 console.log("Ok. You quit the app.");
+
+function listTodos() {
+    console.log("******")
+    todos.forEach(function (todo, i) {
+        console.log(i + ": " + todo);
+    });
+    console.log("******")
+}
+
+function addTodo() {
+    //ask for new todo
+    var newTodo = prompt("Enter new To Do");
+    //add to todos array
+    todos.push(newToDo);
+    console.log("Added ToDo.")
+}
+
+function deleteTodo() {
+    //ask for index of todo to be deleted
+    var index = prompt("Enter index of ToDO to delete.")
+    //delete that todo
+    //splice
+    todos.splice(index, 1);
+    console.log("Deleted ToDo")
+}
